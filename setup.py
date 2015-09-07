34fdfacafbcb1f3476168b381969cbd5d2803e56
Braulio Valdivielso Martínez <brlvldvlsmrtnz@gmail.com>
2015-09-08 00:39:49 +0200

    Added freqens command

--- setup.py
+++ setup.py
@@ -104,6 +104,7 @@ setup(
     # pip to create the appropriate form of executable for the target platform.
     entry_points={
         'console_scripts': [
+            "freqens=freqens.commands:main"
         ],
     },
 )
